4cef55143c23f973ac110000a5b7a90a3b23b4a8
sarazwairi <sara.zwairi@gmail.com>
2021-04-04 13:54:28 +0300

    add loops

--- javascript/app.js
+++ javascript/app.js
@@ -2,15 +2,20 @@
 
 var username = prompt("what's your name?");
 
+function welcomeMessage(username) {
+    console.log(username);
+}
     alert("welcome " + username + " to the color theory page");
 
-console.log(username);
+
 
 
 // second prompt
 
 var color = prompt("what's your favourite theory ? please choose between: C -> complementary colors, T -> trindic colors ,A ->analogous color");
-
+while (color!=='T'&&color!=='C'&&color!=='A') {
+    color=prompt("please choose T,C,A and upper case only");
+}
 console.log(color);
 
 
@@ -19,8 +24,9 @@ console.log(color);
 // third prompt
 
 var numberOfTheorys = prompt("How many times you want us to see your favourite theory?");
-   
-p = '' ;
+
+var p = '' ;
+var result='' ;  
 
     if (color == 'C') {
         p = ('<img src="picture/1.png" >');
@@ -29,12 +35,14 @@ p = '' ;
     } else if (color == 'A') {
         p = ('<img src="picture/2.png" >');
     }
+    for (var i=0;i<numberOfTheorys;i++) {
+    result+=p
+    } 
    
-   
-    document.write('<h3>' + "Hello" + username + '</h3>');
+    document.write('<h3>' + "Hello " + username + '</h3>');
        
-    document.write('<h2>' + "Your favourite theory is:" + color + '</h2>');
+    document.write('<h2>' + "Your favourite theory is:" + p + '</h2>');
 
     document.write('<h2>' + "Number of theorys is:" + numberOfTheorys + '</h2>');
      
-    document.write(p)
+    document.write(result)
