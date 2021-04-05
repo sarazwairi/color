72466963fa33481e6e75b69aad12ff3ca8860acd
sarazwairi <sara.zwairi@gmail.com>
2021-04-05 14:21:26 +0300

    css

--- javascript/app.js
+++ javascript/app.js
@@ -1,48 +1,48 @@
-// first prompt
-
-var username = prompt("what's your name?");
-
-function welcomeMessage(username) {
-    console.log(username);
-}
-    alert("welcome " + username + " to the color theory page");
-
-
-
-
-// second prompt
-
-var color = prompt("what's your favourite theory ? please choose between: C -> complementary colors, T -> trindic colors ,A ->analogous color");
-while (color!=='T'&&color!=='C'&&color!=='A') {
-    color=prompt("please choose T,C,A and upper case only");
-}
-console.log(color);
-
-
-
-
-// third prompt
-
-var numberOfTheorys = prompt("How many times you want us to see your favourite theory?");
-
-var p = '' ;
-var result='' ;  
-
-    if (color == 'C') {
-        p = ('<img src="picture/1.png" >');
-    } else if (color == 'T') {
-        p = ('<img src="picture/3.png" >');
-    } else if (color == 'A') {
-        p = ('<img src="picture/2.png" >');
+for (var n = 0; n < 3; n++) {
+    
+    alert("welcome to the color theory page");
     }
-    for (var i=0;i<numberOfTheorys;i++) {
-    result+=p
-    } 
-   
-    document.write('<h3>' + "Hello " + username + '</h3>');
+    
+    
+    var username = prompt("what's your name?");
+    
+    
+    var color = prompt("what's your favourite theory ? please choose between: C -> complementary colors, T -> trindic colors ,A ->analogous color");
+    
+    function favCol() {
+    
+        while (color!== 'T' && color !== 'C' && color !== 'A') {
+            color = prompt("please choose T,C,A and upper case only");
+        }
+    
+    }
+    
+    
+    favCol();
+    
+    
+    var numberOfTheorys = prompt("How many times you want us to see your favourite theory?");
+    
+    var favTheo=function() {
        
-    document.write('<h2>' + "Your favourite theory is:" + p + '</h2>');
-
-    document.write('<h2>' + "Number of theorys is:" + numberOfTheorys + '</h2>');
-     
-    document.write(result)
+        var p = '';
+        var result = '';
+    
+        if (color == 'C') {
+            p = ('<img src="picture/1.png" >');
+    
+        } else if (color == 'T') {
+    
+            p = ('<img src="picture/3.png" >');
+    
+        } else if (color == 'A') {
+    
+            p = ('<img src="picture/2.png" >');
+        }
+    
+        for (var i = 0; i < numberOfTheorys; i++) {
+            result += p
+        }
+            return result
+    }
+    
